98e864ad57a474d26e1344a725a31a6250a5e258
landrescastillo84 <landrescastillo84@gmail.com>
2025-05-08 18:45:26 -0500

    variable

---
+++ ESTUDIANTE/03_variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  /// variable [var] - es una palabra reservada
+ var nombre = "andres";
+//[const]- constante
+const velocidadluz = "30000000";
+// [final] - inmutable
+final luzfinal = "300000";
+
+// diferencias
+nombre = "1";
+nombre = "2";
+nombre = "3";
+nombre = "4";
+
+
+ } 
+ 
---
+++ ESTUDIANTE/04_tipos_de_variables.dart
@@ -0,0 +1,29 @@
+void main (){
+  //tipo de dato string - clase de dart
+  String apellido = "castillo";
+
+String mensaje = 'hola'"mundo";
+String mensaje2 = "hola'Mundo'";
+String mensaje3 = mensaje + mensaje2;
+print(mensaje3);
+
+// concatenacion con el signo $
+
+String mensaje5 = "$mensaje $mensaje2";
+print(mensaje5);
+num edad = 5;
+edad = 5.2;
+int mes = 3;
+mes = 4;
+double precio = 100.24;
+print (precio);
+precio = 1;
+print (precio);
+
+final preciofixed = precio.toStringAsFixed(3);
+print(preciofixed);
+
+string valorString = "10.1"
+
+
+}
