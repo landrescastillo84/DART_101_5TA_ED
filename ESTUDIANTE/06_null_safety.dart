f29c130d696ad113e6441726aecede06e4502660
landrescastillo84 <landrescastillo84@gmail.com>
2025-05-12 20:20:13 -0500

    mapas

---
+++ ESTUDIANTE/06_null_safety.dart
@@ -0,0 +1,53 @@
+void main() {
+
+final restaurantes = {
+  "restaurante 1":{
+    "direccion": "latacunga",
+    "menu":{
+      "fuerte": "carne",
+    },
+  },
+
+  "restaurante 2":{
+      "direccion": "Quito",
+    "menu":{
+       "fuerte": "Encebollado",
+    },
+  },
+
+"restaurante 3":{
+      "direccion": "Cuenca",
+    "menu":{
+      "fuerte":"cuy",
+    },
+},
+
+};
+
+//final fuerte1 = restaurantes ["restaurante3"]?["menu"]!;
+//print(fuerte1);
+final fuerte2 = restaurantes["restaurantes3"]?["menu"];
+print(fuerte2);
+
+String? nombre = null;
+  int? edad = null;
+  double? salario = null;
+  List<String>? lista = null;
+  Map<String, String>? mapa = null;
+void main() {
+  // Variables con valores null
+  String? nombre = null;
+  int? edad = null;
+  double? salario = null;
+  List<String?>? lista = null;
+  Map<String, String>? mapa = null;
+
+  // Imprimir los valores
+  print('Nombre: $nombre');
+  print('Edad: $edad');
+  print('Salario: $salario');
+  print('Lista: $lista');
+  print('Mapa: $mapa');
+}
+  // tipo de dato definir, el mismo tipo de dato
+}
