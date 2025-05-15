4aefc2b2de8efd66d039cee453b376cfbbd0b046
landrescastillo84 <landrescastillo84@gmail.com>
2025-05-14 19:06:49 -0500

    operadores

---
+++ ESTUDIANTE/07_operadores.dart
@@ -0,0 +1,24 @@
+void main (){
+final valor1 = 1;
+final valor2 = 2;
+
+final suma= valor1 + valor2;
+print (suma);
+
+final resta = valor1 - valor2;
+print (resta);
+
+final multiplicacion = valor1 * valor2;
+print (multiplicacion);
+final division = valor1 / valor2;
+print (division);
+
+
+  // operadores son: 
+  //suma
+
+
+
+
+
+}
