d7ebdf339810804a92b5d9344f5a05c944598656
landrescastillo84 <landrescastillo84@gmail.com>
2025-05-18 20:04:24 -0500

    for dart

--- ESTUDIANTE/09_for.dart
+++ ESTUDIANTE/09_for.dart
@@ -9,23 +9,15 @@ print ("el decremento es: $i");
 final lista = [0,1,2,3,4,5];
 for(var numero in lista) {
   print ("For in: $numero");
-
 }
 }
-//final x = lista.map((pepito){
-  //print("mi dia map es $pepito");
-  //return pepito + 1;
 
-//});
-//print(x);
-for (var i = 0; i < 5; i++) {
-
- if (i==3){
-  continue;
- } 
- if(i==3){
-  print(""valor es: $i);
-  break;
+// tengo una lista
+//["lunes", "martes", "miercoles"]
+//for(var i)
+final dias = ["lunes", "martes", "miercoles"];
+for (var i = 0; i<=dias.length ; i++) {
+  print (dias [i]);
 }
 
 
